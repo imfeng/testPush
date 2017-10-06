b375a010faf3f7bff85f95ee7c39aae7ea4ce86c
imfeng <hermitf@big-cause.com>
2017-10-06 13:07:19 +0800

    home status

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController ,Platform} from 'ionic-angular';
+import { FCM } from '@ionic-native/fcm';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  fcm_status = {
+    "onNotification" : false,
+    "wasTapped" : false,
+    "NotificationData" : 'none'
 
-  constructor(public navCtrl: NavController) {
+  }
+  constructor(
+    private fcm: FCM,
+   private platform : Platform,
+    public navCtrl: NavController) {
+
+      this.platform.ready().then(() => {
+      
+      this.fcm.onNotification().subscribe(data=>{
+        this.fcm_status.onNotification = true;
+        if(data.wasTapped){
+          this.fcm_status.wasTapped = true;
+          console.log(JSON.stringify(data));
+        } else {
+          this.fcm_status.wasTapped = false;
+          console.log(">>> Received in foreground");
+          console.log(JSON.stringify(data));
+        };
+        this.fcm_status.NotificationData = JSON.stringify(data);
+      })
+      
+
+      
+      //this.fcm.unsubscribeFromTopic('marketing');
+  
+  
+  
+      });
 
   }
 

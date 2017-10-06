39509d33b5f8af18e9fd950d389c65dcb73c4c9f
imfeng <hermitf@big-cause.com>
2017-10-06 12:21:54 +0800

    FCM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,27 +5,39 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { Test1Page } from '../pages/test1/test1';
+import { Test2Page } from '../pages/test2/test2';
+
+import { FCM } from '@ionic-native/fcm';
+
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
-
+  fcmInfo = {
+    "token": '0',
+    'data': '0'
+  }
   rootPage: any = HomePage;
-
+  
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+    private fcm: FCM,
+    public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Test1 Page', component: Test1Page },
+      { title: 'Test2 Page', component: Test2Page }
     ];
-
   }
+  
 
   initializeApp() {
     this.platform.ready().then(() => {
@@ -33,6 +45,36 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      
+    this.fcm.subscribeToTopic('marketing');
+    this.fcm.getToken().then(token=>{
+      console.log('>>> getToken() :\n'+ JSON.stringify(token));
+      this.fcmInfo.token = token;
+      //backend.registerToken(token);
+    })
+    
+    this.fcm.onNotification().subscribe(data=>{
+      if(data.wasTapped){
+        console.log(">>> Received in background");
+        console.log(JSON.stringify(data));
+      } else {
+        console.log(">>> Received in foreground");
+        console.log(JSON.stringify(data));
+      };
+      this.fcmInfo.data = JSON.stringify(data);
+    })
+    
+    this.fcm.onTokenRefresh().subscribe(token=>{
+      console.log('>>> onTokenRefresh() :\n'+ JSON.stringify(token));
+      this.fcmInfo.token = token;
+      //backend.registerToken(token);
+    })
+    
+    //this.fcm.unsubscribeFromTopic('marketing');
+
+
+
     });
   }
 
@@ -41,4 +83,6 @@ export class MyApp {
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
   }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,39 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { Test1Page } from '../pages/test1/test1';
+import { Test2Page } from '../pages/test2/test2';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { FCM } from '@ionic-native/fcm';
+
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    Test1Page,
+    Test2Page,
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    Test1Page,
+    Test2Page,
   ],
   providers: [
+    FCM,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
